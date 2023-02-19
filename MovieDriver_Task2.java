fce8ddd8376072fdd387c8f7744f7600b2186b8e
HamzaShahCS <123704176+HamzaShahCS@users.noreply.github.com>
2023-02-18 23:21:47 -0500

    Add files via upload

---
+++ MovieDriver_Task2.java
@@ -0,0 +1,52 @@
+/*
+ * Class: CMSC203 
+ * Instructor: Ashique Tanveer
+ * Description: This class tests the Movie class to ensure correct output based on input
+ * Due: 02/20/2023
+ * Platform/compiler: Eclipse
+ * I pledge that I have completed the programming 
+ * assignment independently. I have not copied the code 
+ * from a student or any source. I have not given my code 
+ * to any student.
+   Print your Name here: Hamza Shah
+ */
+
+import java.util.Scanner;
+
+public class MovieDriver_Task2{
+
+	public static void main(String[] args) 
+	{
+		Scanner keyboard = new Scanner(System.in);
+		Movie userMovie = new Movie();
+		String userResponse = "y";
+
+		while (userResponse.equalsIgnoreCase("y"))
+		{
+			System.out.println("Enter the name of a movie");
+			String userTitle = keyboard.nextLine();
+			userMovie.setTitle(userTitle);
+
+			System.out.println("Enter the rating of the movie");
+			String userRating= keyboard.nextLine();
+			userMovie.setRating(userRating);
+
+			System.out.println("Enter the number of tickets sold for this movie");	
+			int userSoldTickets = keyboard.nextInt();
+			userMovie.setSoldTickets(userSoldTickets);
+
+			System.out.println(userMovie.toString());
+
+			System.out.println("Do you want to enter another? (y or n)");
+			keyboard.nextLine();
+			userResponse = keyboard.nextLine();
+		}
+
+
+		System.out.print("Goodbye");
+
+		keyboard.close();
+
+	}
+
+}
